e8f484fb294d503c1381432aa00c7689da63be81
Dark Templar <xuyi556677@163.com>
2015-11-30 23:31:29 +0800

    add shell script

---
+++ bin/kmeans-example.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+JARFILES=""
+for f in ../target/dependency/*.jar; do
+  JARFILES=${JARFILES}":"$f
+done
+
+java -cp $JARFILES:../target/classification.jar xof.classification.kmeans.KMeansAlgorithm
+
